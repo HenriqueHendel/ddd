56f0d79ad4dbff66ded2747502ffe247ce8df114
Henrique Lopes <henriquehendell@gmail.com>
2025-09-01 22:39:05 -0300

    feat: create routes to create and list customers

---
+++ src/infrastructure/api/routes/customer.routes.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from 'express'
+import { CreateCustomerUseCase } from '../../../usecases/customer/create/create.customer.usecase'
+import CustomerRepository from '../../customer/repository/sequelize/customer.repository'
+import { ListCustomerUseCase } from '../../../usecases/customer/list/list.customer.usecase'
+
+export const customerRoute = express.Router()
+
+customerRoute.post('/', async (req: Request, res: Response) => {
+  const repository = new CustomerRepository()
+  const useCase = new CreateCustomerUseCase(repository)
+
+  try {
+    const customerDto = {
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        number: req.body.address.number,
+        city: req.body.address.city,
+        zip: req.body.address.zip,
+      },
+    }
+
+    const output = await useCase.execute(customerDto)
+
+    res.send(output)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+customerRoute.get('/', async (req: Request, res: Response) => {
+  const repository = new CustomerRepository()
+  const useCase = new ListCustomerUseCase(repository)
+
+  try {
+    const output = await useCase.execute()
+
+    res.send(output)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
