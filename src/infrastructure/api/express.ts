752a5695478cb0c0b088aca187bd886a6af3f38e
Henrique Lopes <henriquehendell@gmail.com>
2025-09-01 22:53:39 -0300

    feat: add product routes on express

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -2,10 +2,13 @@ import express, { Express } from 'express'
 import { Sequelize } from 'sequelize-typescript'
 import CustomerModel from '../customer/repository/sequelize/customer.model'
 import { customerRoute } from './routes/customer.routes'
+import { productsRoute } from './routes/products.routes'
+import ProductModel from '../product/repository/sequelize/product.model'
 
 export const app: Express = express()
 app.use(express.json())
 app.use('/customer', customerRoute)
+app.use('/products', productsRoute)
 
 export let sequelize: Sequelize
 
@@ -16,7 +19,7 @@ async function setupDb() {
     logging: false,
   })
 
-  sequelize.addModels([CustomerModel])
+  sequelize.addModels([CustomerModel, ProductModel])
   await sequelize.sync()
 }
 
