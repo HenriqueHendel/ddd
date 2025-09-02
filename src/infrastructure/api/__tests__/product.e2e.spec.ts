e99438fc409d7262ce3066367fbc24871ac311d1
Henrique Lopes <henriquehendell@gmail.com>
2025-09-01 22:53:19 -0300

    test: routes to create and list products

---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,56 @@
+import { sequelize, app } from '../express'
+import request from 'supertest'
+
+describe('End to end test for product', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+  })
+
+  afterAll(async () => {
+    await sequelize.close()
+  })
+
+  it('should create a product', async () => {
+    const response = await request(app).post('/products').send({
+      name: 'Product',
+      price: 100,
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body.name).toBe('Product')
+    expect(response.body.price).toBe(100)
+  })
+
+  it('should not create a product', async () => {
+    const response = await request(app).post('/products').send({
+      name: 'Henrique',
+    })
+
+    expect(response.status).toBe(500)
+  })
+
+  it('should list all products', async () => {
+    await request(app).post('/products').send({
+      name: 'Product 1',
+      price: 100,
+    })
+
+    await request(app).post('/products').send({
+      name: 'Product 2',
+      price: 200,
+    })
+
+    const listResponse = await request(app).get('/products').send()
+
+    expect(listResponse.status).toBe(200)
+    expect(listResponse.body.products.length).toBe(2)
+
+    const product1 = listResponse.body.products[0]
+    expect(product1.name).toBe('Product 1')
+    expect(product1.price).toBe(100)
+
+    const product2 = listResponse.body.products[1]
+    expect(product2.name).toBe('Product 2')
+    expect(product2.price).toBe(200)
+  })
+})
